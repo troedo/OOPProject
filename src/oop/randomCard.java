c1d3e5a926546a39aca5c36a21296df51f1bbdf5
ASUS <ASUS@10.94.32.157>
2018-11-30 13:40:15 +0700

    init

---
+++ src/oop/randomCard.java
@@ -0,0 +1,71 @@
+package oop;
+public class randomCard {
+    void Random(int x){
+        if(x == 0){
+            new a1().setVisible(true);
+        }
+        if(x == 1){
+            new a2().setVisible(true);
+        }
+        if(x == 2){
+            new a3().setVisible(true);
+        }
+        if(x == 3){
+            new a4().setVisible(true);
+        }
+        if(x == 4){
+            new a5().setVisible(true);
+        }
+        if(x == 5){
+            new a6().setVisible(true);
+        }
+        if(x == 6){
+            new a7().setVisible(true);
+        }
+        if(x == 7){
+            new a8().setVisible(true);
+        }
+        if(x == 8){
+            new a9().setVisible(true);
+        }
+        if(x == 9){
+            new a10().setVisible(true);
+        }
+        if(x == 10){
+            new a11().setVisible(true);
+        }
+        if(x == 11){
+            new a12().setVisible(true);
+        }
+        if(x == 12){
+            new a13().setVisible(true);
+        }
+        if(x == 13){
+            new a14().setVisible(true);
+        }
+        if(x == 14){
+            new a15().setVisible(true);
+        }
+        if(x == 15){
+            new a16().setVisible(true);
+        }
+        if(x == 16){
+            new a17().setVisible(true);
+        }
+        if(x == 17){
+            new a18().setVisible(true);
+        }
+        if(x == 18){
+            new a19().setVisible(true);
+        }
+        if(x == 19){
+            new a20().setVisible(true);
+        }
+        if(x == 20){
+            new a21().setVisible(true);
+        }
+        if(x == 21){
+            new a22().setVisible(true);
+        }
+    }
+}
